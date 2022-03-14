4c8cc7546fd6f5a6b592bce7c03906e47ad9499f
Vedad Karalić <61968590+Vedoxx@users.noreply.github.com>
2022-03-14 11:56:25 +0100

    Add files via upload

---
+++ taskOne.java
@@ -0,0 +1,8 @@
+public class taskOne {
+    public static void main(String[] args) {
+            Numbers number = new Numbers();
+            number.setSize();
+            number.setNumbers();
+            number.arrayInformation();
+    }
+    }

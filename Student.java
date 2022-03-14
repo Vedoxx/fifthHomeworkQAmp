e41fcfcfa3be220458e8335909d5d62d85629ba5
Vedad Karalić <61968590+Vedoxx@users.noreply.github.com>
2022-03-14 17:02:42 +0100

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,62 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Student {
+    Scanner object = new Scanner(System.in);
+    Integer number;
+    String name;
+    HashMap<Integer, String> studentinfo = new HashMap<>();
+
+    Student(Integer number, String name) {
+        this.number = number;
+        this.name = name;
+        this.studentinfo.put(this.number, this.name);
+    }
+
+    public void addStudent(Integer number, String name) {
+        this.number = number;
+        this.name = name;
+        studentinfo.put(this.number, this.name);
+    }
+
+    public void allStudentNames() {
+        System.out.print("Students:");
+        for (Map.Entry<Integer, String> entry : studentinfo.entrySet()) {
+            System.out.print(entry.getValue()+" ");
+        }
+    }
+
+    public void allStudentIndexes() {
+        for (Map.Entry<Integer, String> entry : studentinfo.entrySet()) {
+            System.out.print("Student indexes:");
+            System.out.println(entry.getKey());
+        }
+    }
+
+    public void addStudent() {
+        object.nextLine();
+        System.out.print("Enter name of student:");
+        String name = object.nextLine();
+        this.name = name;
+        System.out.print("Enter index for the new student:");
+        Integer number = object.nextInt();
+        this.number = number;
+        studentinfo.put(this.number, this.name);
+        System.out.println("New student is added");
+        System.out.println(studentinfo);
+    }
+
+    public void changeStudent() {
+        System.out.println("Enter index of the student and new name for the student:");
+        studentinfo.replace(object.nextInt(),object.next());
+        System.out.println("Student new data"+studentinfo);
+    }
+  public void eraseElement(){
+      System.out.println("Enter index of the student you want to remove:");
+      Integer index=object.nextInt();
+      this.number=index;
+      studentinfo.remove(index);
+      System.out.println("Student is removed!");
+  }
+}
